af1c3264879c3f2b95afdf60f3af4d092ef51bbe
Dvaryteacher123 <dullamanyama0@gmail.com>
2026-09-07 20:58:17 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,9 @@
+
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+
 // Taarifa za mradi wako wa Supabase
 const supabaseUrl = 'https://mhglcqdmauflvmxfzswe.supabase.co';
 const supabaseKey = 'sb_publishable_dBisUdMYLPbmZpQQN74Dkg_s3pX6n2S';
 
-// Kuunganisha Supabase client kwenye app yako
-const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
+// Kuunganisha na kusafirisha Supabase client ili itumike kwenye index.html na sehemu zingine
+export const supabase = createClient(supabaseUrl, supabaseKey);
