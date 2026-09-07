7abad1638db73dde520293ea745821bc521a6756
Dvaryteacher123 <dullamanyama0@gmail.com>
2026-09-07 19:45:54 +0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+// Taarifa za mradi wako wa Supabase
+const supabaseUrl = 'https://mhglcqdmauflvmxfzswe.supabase.co';
+const supabaseKey = 'sb_publishable_dBisUdMYLPbmZpQQN74Dkg_s3pX6n2S';
+
+// Kuunganisha Supabase client kwenye app yako
+const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
